589dc96991b9a23c65de5bcb0b23d1e577ad7674
Alexander Ursu <alexander.ursu@gmail.com>
2018-06-20 00:32:46 +0200

    Added class javalocal::java8

---
+++ manifests/java8.pp
@@ -0,0 +1,44 @@
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include javalocal::java8
+class javalocal::java8 (
+    Lsys::Java8Major
+            $version_major = $javalocal::version_major,
+    Lsys::Java8Minor
+            $version_minor = $javalocal::version_minor,
+    String  $url_hash      = $javalocal::url_hash,
+    Enum['jre', 'jdk']
+            $java_se       = $javalocal::java_se,
+    Stdlib::Unixpath
+            $system_path   = $javalocal::system_path,
+    Optional[String]
+            $install_path  = $javalocal::install_path,
+    Boolean $control_java  = $javalocal::control_java,
+)
+{
+    if $install_path {
+        $dist_install_path = $install_path
+    }
+    elsif $version_major =~ /(\d+)u(\d+)/ {
+        $dist_install_path = "${java_se}1.${1}.0_${2}"
+    }
+    else {
+        $dist_install_path = "${java_se}-${version_major}"
+    }
+
+    java::oracle { $dist_install_path:
+        version_major => $version_major,
+        version_minor => $version_minor,
+        url_hash      => $url_hash,
+        java_se       => $java_se,
+    }
+
+    class { 'javalocal::alternatives':
+        java_install_path => $dist_install_path,
+        control_java      => $control_java,
+        require           => Java::Oracle[$dist_install_path],
+    }
+}
