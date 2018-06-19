2376311abdd3623ce2c2df3f3cd4f081f3d995eb
Alexander Ursu <alexander.ursu@gmail.com>
2018-06-20 00:34:36 +0200

    Added some module parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,34 @@
-# A description of what this class does
+# Class: javalocal
 #
-# @summary A short summary of the purpose of this class
+# @summary jalocal clasee required for in-module Hiera initialization
+#
+# Parameters:
+#
+#  [*java_se*]
+#    String. One of 'jdk' or 'jre' depends on Java distribution set
+#
+#  [*install_path*]
+#    String. Default is jdk1.8.0_171. It is directory inside system Java path
+#    which consts of Oracle Java distribution
+#
+#  [*control_java*]
+#    Boolean. Default - true. If set to true than module javalocal will use
+#    class 'java' from standard module puppetlabs/java that could be not
+#    desirable on some environments. Class 'java' is used for setting up java
+#    into OS alternatives system, therefore set control_java flag to false will
+#    disable this feature as well
 #
 # @example
 #   include javalocal
-class javalocal {
+class javalocal (
+    String  $version_major,
+    String  $version_minor,
+    String  $url_hash,
+    String  $java_se,
+    String  $system_path,
+    Optional[String]
+            $install_path,
+    Boolean $control_java,
+)
+{
 }
