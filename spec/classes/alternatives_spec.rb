1273f2e71e3a76467f20bd488b7cebe8e4825af4
Alexander Ursu <alexander.ursu@gmail.com>
2018-06-20 02:41:39 +0200

    Added mised parameter java_install_path

--- spec/classes/alternatives_spec.rb
+++ spec/classes/alternatives_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
 
 describe 'javalocal::alternatives' do
+  let(:pre_condition) { 'include javalocal' }
+
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+      let(:params) { { 'java_install_path' => 'jdk1.8.0_171' } }
 
       it { is_expected.to compile }
     end
