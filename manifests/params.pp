50f81b15bdea8f291d2cbc9bdbae6a03c003f06f
Alexander Ursu <alexander.ursu@gmail.com>
2018-08-23 22:55:45 +0200

    remove default valure for installation path

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,7 +9,6 @@ class javalocal::params {
     $java_version_minor         = 'b13'
     $java_url_hash              = '96a7b8442fe848ef90c96a2fad6ed6d1'
     $java_se                    = 'jdk'
-    $java_install_path          = 'jdk1.8.0_181'
     $java_alternative           = "java-8-oracle-${::architecture}"
     $java_alternative_prio      = 2071
     $java_alternative_template  = 'javalocal/debian-alternatives.pp'
