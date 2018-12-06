20907d8a9ac393f0c420a1dd7f09d28cd0477f6e
Alexander Ursu <alexander.ursu@gmail.com>
2018-12-06 08:59:39 +0100

    Updated default Java 8 version to latest

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,9 +5,9 @@
 # @example
 #   include javalocal::params
 class javalocal::params {
-    $java_version_major         = '8u181'
-    $java_version_minor         = 'b13'
-    $java_url_hash              = '96a7b8442fe848ef90c96a2fad6ed6d1'
+    $java_version_major         = '8u191'
+    $java_version_minor         = 'b12'
+    $java_url_hash              = '2787e4a523244c269598db4e85c51e0c'
     $java_se                    = 'jdk'
     $java_alternative           = "java-8-oracle-${::architecture}"
     $java_alternative_prio      = 2071
