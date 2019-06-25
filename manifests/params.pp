9dc92277e379eb6cf5b91499a09d620b34f162c4
Alexander Ursu <alexander.ursu@gmail.com>
2019-06-26 00:46:21 +0200

    Update Java to version 8u211

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,11 +5,11 @@
 # @example
 #   include javalocal::params
 class javalocal::params {
-    $java_version_major         = '8u191'
+    $java_version_major         = '8u211'
     $java_version_minor         = 'b12'
-    $java_url_hash              = '2787e4a523244c269598db4e85c51e0c'
+    $java_url_hash              = '478a62b7d4e34b78b671c754eaaf38ab'
     $java_se                    = 'jdk'
     $java_alternative           = "java-8-oracle-${::architecture}"
     $java_alternative_prio      = 2071
-    $java_alternative_template  = 'javalocal/debian-alternatives.pp'
+    $java_alternative_template  = 'javalocal/debian-alternatives.erb'
 }
