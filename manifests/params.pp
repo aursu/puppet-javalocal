a39fba6c847676439e71462566e6b1853ea3c3cc
Alexander Ursu <alexander.ursu@gmail.com>
2020-08-10 16:17:14 +0200

    Updated version to 8u261

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,9 +5,9 @@
 # @example
 #   include javalocal::params
 class javalocal::params {
-    $java_version_major         = '8u221'
-    $java_version_minor         = 'b11'
-    $java_url_hash              = '230deb18db3e4014bb8e3e8324f81b43'
+    $java_version_major         = '8u261'
+    $java_version_minor         = 'b12'
+    $java_url_hash              = 'a4634525489241b9a9e1aa73d9e118e6'
     $java_se                    = 'jdk'
     $java_alternative           = "java-8-oracle-${::architecture}"
     $java_alternative_prio      = 2071
